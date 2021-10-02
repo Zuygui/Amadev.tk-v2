e3788db58f91c3fb5a53e357e68affb9d194bcf2
Zuygui <maxime@halbitte.fr>
2021-10-02 13:26:54 +0200

    Sortie de l'usine

--- assets/js/Newsletter.js
+++ assets/js/Newsletter.js
@@ -1,7 +1,6 @@
 function sendNews()
 {
-  webhook()
-  alert('Vous êtes bien inscrit a la newsletter !')
+  alert('Oh non ! Nous rencontrons des problèmes avec les webhooks de discord afin de vous inscrire... Nous ferons une annonce a')
 }
 
 function webhook()
@@ -9,11 +8,19 @@ function webhook()
 
   var email = document.getElementById('newsletter')
   var xhr = new XMLHttpRequest()
-  xhr.open('POST', 'https://discord.com/')
+  xhr.open(
+    'POST',
+    'https://discord.com/api/webhooks/893807655183851520/gqJXKPqKcaT_yNAE6UarXWI2jJacqfFq6IThzFQU81NsyiYXOSEHsAC8QAlh4CgAoj_S'
+  )
   xhr.setRequestHeader('Content-Type', 'application/json')
   xhr.send(JSON.stringify({
     'embeds': [
-      { 'fields': [{ 'inline': false, 'name': 'Un nouvel abboné a la newsletter ! Email:', 'value': email}]}
+      {
+        'fields': [{
+          'inline': false, 'name': 'Un nouvel abboné a la newsletter ! Email:',
+          'value': email
+        }]
+      }
     ]
   }))
 }
