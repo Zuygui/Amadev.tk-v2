7550ce1639a61fa5c99eb9ad17f4794597655860
Zuygui <maxime@halbitte.fr>
2021-10-11 19:23:38 +0200

    v2

--- assets/js/Newsletter.js
+++ assets/js/Newsletter.js
@@ -1,26 +1,21 @@
 function sendNews()
 {
-  alert('Oh non ! Nous rencontrons des problèmes avec les webhooks de discord afin de vous inscrire... Nous ferons une annonce a')
+  alert('Oh non ! Nous rencontrons des problèmes avec les webhooks de discord afin de vous inscrire... Nous ferons une annonce une fois ce systeme finit.')
 }
 
 function webhook()
 {
 
-  var email = document.getElementById('newsletter')
+  var email = document.getElementById('newsletter').innerHTML.toString()
   var xhr = new XMLHttpRequest()
   xhr.open(
-    'POST',
-    'https://discord.com/api/webhooks/893807655183851520/gqJXKPqKcaT_yNAE6UarXWI2jJacqfFq6IThzFQU81NsyiYXOSEHsAC8QAlh4CgAoj_S'
+    "POST",
+    "https://discord.com/api/webhooks/897171558068543588/JV736Ps9fk9n0Bp8UxwZVEYuhMqT6XsYJlgW5VNaoXJgr0_csODBbWdMu-etJ1ugwsUG"
   )
   xhr.setRequestHeader('Content-Type', 'application/json')
-  xhr.send(JSON.stringify({
-    'embeds': [
-      {
-        'fields': [{
-          'inline': false, 'name': 'Un nouvel abboné a la newsletter ! Email:',
-          'value': email
-        }]
-      }
-    ]
-  }))
+  xhr.send(
+    JSON.stringify({
+      embeds: [{ color: 0, title: "Un nouvel abboné a la newletter !", description: `Email: ${email}` }],
+    })
+  )
 }
